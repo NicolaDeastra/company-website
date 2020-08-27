5b071e6488ea668fbd3a23936f052c3f45620e60
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-08-27 16:17:21 +0700

    add pricing component

--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import HeroSection from "../../HeroSection";
 import { homeObjOne, homeObjFour, homeObjThree, homeObjTwo } from "./Data";
+import Pricing from "../../Pricing";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <HeroSection {...homeObjOne} />
       <HeroSection {...homeObjThree} />
       <HeroSection {...homeObjTwo} />
+      <Pricing />
       <HeroSection {...homeObjFour} />
     </>
   );
